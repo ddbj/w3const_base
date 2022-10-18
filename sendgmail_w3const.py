961933b00301c9e192bf73063692b457667eb402
Takehide Kosuge <tkosuge@nig.ac.jp>
2022-10-18 14:04:46 +0900

    Update sendgmail_w3const.py

--- sendgmail_w3const.py
+++ sendgmail_w3const.py
@@ -52,6 +52,7 @@ def chkwhite(e):
         with open(os.environ['HOME'] + '/.sendgmail_w3const/whitelist', 'r', encoding = 'utf-8') as f:
             white = []
             white = f.read().split('\n')
+        white.pop(-1)
         print(white)
         c = 0
         for v in e:
