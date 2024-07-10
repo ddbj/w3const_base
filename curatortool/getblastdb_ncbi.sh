d78c2ba98814cf640e8ff1d355926ffec575365b
Takehide Kosuge <tkosuge@nig.ac.jp>
2024-07-10 09:31:22 +0900

    Down to 400m in speed

--- curatortool/getblastdb_ncbi.sh
+++ curatortool/getblastdb_ncbi.sh
@@ -95,7 +95,7 @@ getjsondb() {
     while [ "$CNT" -le "$MAXTRY" ]; do
       FCHK=""
       # ascp -i ~/.aspera/connect/etc/asperaweb_id_dsa.openssh -T -k1 -l800m anonftp@ftp.ncbi.nlm.nih.gov:blast/db/${FNAME} ./
-      ascp -i /opt/aspera/connect/etc/asperaweb_id_dsa.openssh -T -k1 -l500m anonftp@ftp.ncbi.nlm.nih.gov:blast/db/${FNAME} ./
+      ascp -i /opt/aspera/connect/etc/asperaweb_id_dsa.openssh -T -k1 -l400m anonftp@ftp.ncbi.nlm.nih.gov:blast/db/${FNAME} ./
       # wget -q -T 60 -t 2 --waitretry=30 $FURL
       # wget -q -P ${DATLOC} -T 60 -t 3 --waitretry=30 $FURL
       # wget -q -P ${DATLOC} -T 60 -t 3 --waitretry=30 $FURL.md5
