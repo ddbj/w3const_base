301ae46ba305f544d0e8cc16ec7ca0103617188d
tkosuge <tkosuge@nig.ac.jp>
2025-04-24 10:39:24 +0900

    Removed get-blastdb.sh, get-UniVec.sh

--- curatortool/get-UniVec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SIF="/home/w3const/work-kosuge/constbase.sif"
-LOG="/home/w3const/work-kosuge/log/get-UniVec.log"
-
-[ -e ${LOG} ] || touch ${LOG}
-singularity exec ${SIF} makeUniVec_blastdb.sh > ${LOG}
--- curatortool/get-blastdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SIF="/home/w3const/work-kosuge/constbase.sif"
-LOG="/home/w3const/work-kosuge/log/get-blastdb.log"
-
-[ -e ${LOG} ] || touch ${LOG}
-singularity exec --bind /home/ddbjshare ${SIF} getblastdb_ncbi.sh > ${LOG}
